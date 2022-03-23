dc2d73a521bed321daf92272cee3f51383803a23
HeyTigrou <gautier.allias@viacesi.fr>
2022-03-23 15:55:20 +0100

    update

--- ws/resources/views/edit.blade.php
+++ ws/resources/views/edit.blade.php
@@ -3,8 +3,8 @@
 <h2> Bienvenue sur la page de modification de l'article</h2>
 <form method="POST" action="{{ route('posts.validateUpdate',['id' => $posts->id ]) }}">
         @csrf
-        <input type="text" name="title" placeholder="{{$posts->title}}">
-        <textarea name="content" cols="30" rows="10" placeholder="{{$posts->content}}"></textarea>
+        <input type="text" name="title" placeholder="{{$posts->title}}" value="{{$posts->title}}">
+        <textarea name="content" cols="30" rows="10" placeholder="{{$posts->content}}">{{$posts->title}}</textarea>
         <button type="submit">Modifier</button>
     </form>
     <div class="pull-right">
